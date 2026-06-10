c9da73e89718499f5b987a1a08c9454e1acc9464
mustafanqnq-cmd <mustafanqnq@gmail.com>
2026-06-10 10:21:12 +0300

    Fix formatting of variable assignments

--- setup/gitstart.py
+++ setup/gitstart.py
@@ -1,8 +1,8 @@
 from git import Repo
 import sys
 
-OFFICIAL_UPSTREAM_REPO = sys.argv[1]
-ACTIVE_BRANCH_NAME = sys.argv[2]
+OFFICIAL_UPSTREAM_REPO= sys.argv[1]
+ACTIVE_BRANCH_NAME= sys.argv[2]
 
 repo = Repo.init()
 origin = repo.create_remote("temponame", OFFICIAL_UPSTREAM_REPO)
