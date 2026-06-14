121810f0f316c06fb8aef63b7d7ae7f96cec41d1
mustafanqnq-cmd <mustafanqnq@gmail.com>
2026-06-14 13:00:40 +0300

    Fix typos in updater.py
    Corrected spelling errors in comments and function names.

--- setup/updater.py
+++ setup/updater.py
@@ -4,7 +4,7 @@ import shlex
 from typing import Tuple
 import sys
 
-# if any requirements are cahnged then install that requirement
+# if any requirements are changed then install that requirement
 async def lines_differnce(file1, file2):
     with open(file1) as f1:
         lines1 = f1.readlines()
@@ -37,16 +37,4 @@ async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
     )
 
 
-async def update_requirements(main , test):
-    a, r = await lines_differnce(main, test)
-    try:
-        for i in a:
-            await runcmd(f"pip install {i}")
-            print(f"Succesfully installed {i}")
-    except Exception as e:
-        print(f"Error while installing requirments {str(e)}")
-
-
-loop = asyncio.get_event_loop()
-loop.run_until_complete(update_requirements(sys.argv[1] , sys.argv[2]))
-loop.close()
+async def update_
