a5e97c6cd1a0f823286c4a9e9ac5cac29ff1d111
mustafanqnq-cmd <mustafanqnq@gmail.com>
2026-06-16 11:03:49 +0300

    Update updater.py

--- setup/updater.py
+++ setup/updater.py
@@ -1,41 +1,20 @@
-import asyncio
-import difflib
-import shlex
-from typing import Tuple
-import sys
-
-# if any requirements are changed then install that requirement
-async def lines_differnce(file1, file2):
-    with open(file1) as f1:
-        lines1 = f1.readlines()
-        lines1 = [line.rstrip("\n") for line in lines1]
-    with open(file2) as f2:
-        lines2 = f2.readlines()
-        lines2 = [line.rstrip("\n") for line in lines2]
-    diff = difflib.unified_diff(
-        lines1, lines2, fromfile=file1, tofile=file2, lineterm="", n=0
-    )
-    lines = list(diff)[2:]
-    added = [line[1:] for line in lines if line[0] == "+"]
-    removed = [line[1:] for line in lines if line[0] == "-"]
-    additions = [i for i in added if i not in removed]
-    removedt = [i for i in removed if i not in added]
-    return additions, removedt
-
-
-async def runcmd(cmd: str) -> Tuple[str, str, int, int]:
-    args = shlex.split(cmd)
-    process = await asyncio.create_subprocess_exec(
-        *args, stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
-    stdout, stderr = await process.communicate()
-    return (
-        stdout.decode("utf-8", "replace").strip(),
-        stderr.decode("utf-8", "replace").strip(),
-        process.returncode,
-        process.pid,
-    )
-
-
 async def update_(event):
+    # إرسال رسالة للمستخدم لبدء عملية التحديث
+    msg = await event.edit("⌭ جـارِ فحـص التحديـثات.. انتظر قليلاً ⌭")
+    
+    # تنفيذ أمر سحب التحديثات من مستودع الكود (Git Pull)
+    stdout, stderr, returncode, pid = await runcmd("git pull")
+    
+    # التحقق من نجاح العملية
+    if returncode != 0:
+        await msg.edit(f"⌭ فشـل التحـديث: {stderr} ⌭")
+        return
 
+
+    if "Already up to date." in stdout:
+        await msg.edit("⌭ لا توجد تحديثات جديدة، البوت محدث بالفعل ⌭")
+    else:
+        await msg.edit("⌭ تـم التحديـث بنجـاح! جـاري إعـادة التشغيـل ⌭")
+        # إعادة تشغيل البوت لتطبيق التغييرات
+        import os
+        os.execl(sys.executable, sys.executable, *sys.argv)
